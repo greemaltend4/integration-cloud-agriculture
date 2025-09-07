62cea0ca4c5d12edba4720c68d827c5591162bb1
greemaltend4 <dengestabiy@hotmail.com>
2025-09-07 14:22:10 -0500

    feat: initial commit for index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.get('/', (req, res) => {
+  res.send('Bienvenue sur l\'application Intégration Cloud Agriculture!');
+});
+
+app.listen(port, () => {
+  console.log(`Serveur en écoute sur le port ${port}`);
+});
