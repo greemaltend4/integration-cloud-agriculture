6a124335415a65528da9e5f65997d9db0499fa26
greemaltend4 <dengestabiy@hotmail.com>
2025-09-07 14:22:24 -0500

    Fix typo in welcome message

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
-  res.send('Bienvenue sur l\'application Intégration Cloud Agriculture!');
+  res.send('Bienvenue sur l\'application d\'Intégration Cloud Agriculture!');
 });
 
 app.listen(port, () => {
